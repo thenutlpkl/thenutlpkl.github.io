d5c729d7c70e7b162176f2699f8d49f776721bc6
Your GitHub Username <himeneko@Himes-MacBook-Air.local>
2025-02-10 09:38:09 +0700

    Initial commit for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from 'path'
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
   return {
+    base: '/tanawitch.github.io/',
     plugins: [react()],
     resolve: {
       alias: {
